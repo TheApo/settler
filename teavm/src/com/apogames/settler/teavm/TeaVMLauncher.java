55d541249cbb1c7ca217539d221bad9b01db04d1
TheApo <dirk.aporius@gmail.com>
2026-03-14 21:08:51 +0100

    update and teavm

---
+++ teavm/src/com/apogames/settler/teavm/TeaVMLauncher.java
@@ -0,0 +1,18 @@
+package com.apogames.settler.teavm;
+
+import com.apogames.settler.Constants;
+import com.apogames.settler.Settler;
+import com.github.xpenatan.gdx.teavm.backends.web.WebApplication;
+import com.github.xpenatan.gdx.teavm.backends.web.WebApplicationConfiguration;
+
+public class TeaVMLauncher {
+    public static void main(String[] args) {
+        Constants.IS_HTML = true;
+        WebApplicationConfiguration config = new WebApplicationConfiguration();
+        config.width = 0;
+        config.height = 0;
+        config.antialiasing = true;
+        config.showDownloadLogs = true;
+        new WebApplication(new Settler(), config);
+    }
+}
